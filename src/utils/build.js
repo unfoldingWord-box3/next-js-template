91eacdb99f082cc77e78c60f54c728f0a6882c38
David Stone <david@nnucomputerwhiz.com>
2022-06-14 17:01:55 -0600

    Use next env for version and build number

--- src/utils/build.js
+++ src/utils/build.js
@@ -1,18 +1,3 @@
-import { version } from '../../package.json'
-
 export function getBuildId() {
-  const hash = getCommitHash()
-  const buildId = { version, hash }
-  return buildId
-}
-
-export function getCommitHash() {
-  let commitHash
-
-  try {
-    commitHash = process.env.NEXT_PUBLIC_BUILD_NUMBER
-  } catch (e) {
-    console.error(`getCommitHash() - could not get commit hash`, e)
-  }
-  return commitHash
+  return { version: process.env.NPM_PACKAGE_VERSION, hash: process.env.BUILDNUMBER }
 }
