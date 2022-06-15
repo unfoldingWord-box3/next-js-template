7d28fa4557e82796873c3a1b0c88c502425db915
David Stone <david@nnucomputerwhiz.com>
2022-06-14 18:09:44 -0600

    Use short hash

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   env: {
     NPM_PACKAGE_VERSION: process.env.npm_package_version,
     BUILD_NUMBER: require('child_process')
-      .execSync('git rev-parse HEAD')
+      .execSync('git rev-parse --short HEAD')
       .toString().trim(),
   }
 }
