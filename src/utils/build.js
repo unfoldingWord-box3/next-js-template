28cd456403842ac0cbf2b670eeb533e3370e1010
David Stone <david@nnucomputerwhiz.com>
2022-06-14 18:04:57 -0600

    fix typo

--- src/utils/build.js
+++ src/utils/build.js
@@ -1,3 +1,3 @@
 export function getBuildId() {
-  return { version: process.env.NPM_PACKAGE_VERSION, hash: process.env.BUILDNUMBER }
+  return { version: process.env.NPM_PACKAGE_VERSION, hash: process.env.BUILD_NUMBER }
 }
